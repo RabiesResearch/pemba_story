a10e9433ad3d6e435ee344a1dfd21afe50406866
Malavika Rajeev <mrajeev08@gmail.com>
2021-05-19 17:30:29 -0700

    ditching the pudding bits

--- src/js/main.js
+++ src/js/main.js
@@ -19,16 +19,6 @@ function resize() {
   }
 }
 
-function setupStickyHeader() {
-  const $header = $body.select('header');
-  if ($header.classed('is-sticky')) {
-    const $menu = $body.select('#slide__menu');
-    const $toggle = $body.select('.header__toggle');
-
-    modalSetup($toggle, $toggle, $header, $menu, 'a, button, .logo', true);
-  }
-}
-
 function init() {
   // adds rel="noopener" to all target="_blank" links
   linkFix();
@@ -36,12 +26,8 @@ function init() {
   $body.classed('is-mobile', isMobile.any());
   // setup resize event
   window.addEventListener('resize', debounce(resize, 150));
-  // setup sticky header menu
-  // setupStickyHeader();
   // kick off graphic code
   graphic.init();
-  // load footer stories
-  // footer.init();
 }
 
 init();
