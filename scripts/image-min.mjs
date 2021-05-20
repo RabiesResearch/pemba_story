3f1c52239f54a780365c79f5675c904f3e392e3f
Malavika Rajeev <mrajeev08@gmail.com>
2021-05-19 18:41:21 -0700

    update dev version

--- scripts/image-min.mjs
+++ scripts/image-min.mjs
@@ -1,6 +1,6 @@
-const imagemin = require('imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
+import * as imagemin from 'imagemin';
+import * as imageminMozjpeg from 'imagemin-mozjpeg';
+import * as imageminPngquant from 'imagemin-pngquant';
 
 async function run(dir) {
   const input = [`dist/assets/${dir}/*.{jpg,png}`];
